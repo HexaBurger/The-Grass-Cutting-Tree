9af5137b9a6be927c1167500e7c98eea168bbf57
HexaBurger <102750443+HexaBurger@users.noreply.github.com>
2023-01-13 10:51:31 +0100

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -44,10 +44,10 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-	/*gain = gain.mul(buyableEffect("lv",11))
+	gain = gain.mul(buyableEffect("lv",11))
 	gain = gain.mul(buyableEffect("lv",13))
 	gain = gain.mul(buyableEffect("lv",14))
-	gain = gain.mul(buyableEffect("pk",11))*/
+	gain = gain.mul(buyableEffect("pk",11))
 	return gain
 }
 
